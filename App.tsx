4d759ba48fdf256c373981e8e000df4feec94bd0
Nimai360 <nimai360@hotmail.com>
2023-11-13 23:34:39 -0300

    2 telas finalizadas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import React, { useState } from "react";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { CheckBox } from 'react-native-elements';
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(true);
+
+  const [day, setDay] = useState('Dia');
+  const [month, setMonth] = useState(null);
+  const [year, setYear] = useState(null);
+
+  const days = Array.from({ length: 31 }, (_, i) => i + 1);
+  const months = Array.from({ length: 12 }, (_, i) => i + 1);
+  const years = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <View style={styles.container}>
       <Image
@@ -34,17 +53,70 @@ export default function App() {
         <Text style={styles.text_info}>
           SENHA<Text style={styles.required}> *</Text>
         </Text>
-        <TextInput style={styles.input}></TextInput>
+        {/* <View style={styles.password_div}> */}
+        <TextInput
+          style={styles.input}
+          secureTextEntry={showPassword}
+          value={password}
+          onChangeText={setPassword}
+        ></TextInput>
+        {/* <MaterialCommunityIcons
+            name={showPassword ? 'eye-off' : 'eye'}
+            style={styles.icon}
+            onPress={toggleShowPassword}
+          />
+        </View> */}
+      </View>
+
+      <View style={styles.div_text_input}>
+        <Text style={styles.text_info}>
+          DATA DE NASCIMENTO<Text style={styles.required}> *</Text>
+        </Text>
+        <View style={styles.date_Div}>
+          <TextInput
+            style={styles.dropList}
+            placeholder="Dia"
+            keyboardType="numeric"
+            placeholderTextColor="#aaa"
+          ></TextInput>
+          <TextInput
+            style={styles.dropList}
+            placeholder="Mês"
+            keyboardType="numeric"
+            placeholderTextColor="#aaa"
+          ></TextInput>
+          <TextInput
+            style={styles.dropList}
+            placeholder="Ano"
+            keyboardType="numeric"
+            placeholderTextColor="#aaa"
+          ></TextInput>
+        </View>
+      </View>
+
+      <View
+        style={styles.checkbox_Div}>
+        <View
+          style={styles.checkBox}>
+
+        </View>
+        <Text style={styles.opcionalCheckBoxText}>(Opcional) Tudo bem me mandar e-mails com atualizações do Discord, dicas e ofertas especiais. Você pode mudar isso a qualquer momento.</Text>
       </View>
 
       <TouchableOpacity style={styles.div_text_button}>
-        <Text style={styles.button}>Entrar</Text>
+        <Text style={styles.button}>Continuar</Text>
       </TouchableOpacity>
+
       <View style={styles.criarConta_DIV}>
-        <Text style={styles.criarConta}>
-          Precisando de uma conta?{" "}
-          <Text style={styles.text_link}>Registre-se</Text>
+        <Text style={styles.termos_text}>
+        Ao se registrar, você concorda com os 
+          <Text style={styles.text_link_termos}>  termos de serviço</Text> e a
+          <Text style={styles.text_link_termos}>  política de privacidade</Text> do Discord.
         </Text>
+      </View>
+
+      <View style={styles.jaTemUmaConta_DIV}>
+          <Text style={styles.text_link}>Já tem uma conta?</Text>
       </View>
     </View>
   );
--- styles.ts
+++ styles.ts
@@ -1,98 +1,168 @@
-import { StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#323339',
-      alignItems: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#323339',
+    alignItems: 'center',
     //   justifyContent: 'flex-start',
-      paddingTop: 50,
-      width: '100%',
-      height: '100%',
-      paddingLeft: 20,
-      paddingRight: 20,
-    },
+    paddingTop: 50,
+    width: '100%',
+    height: '100%',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
 
-    logo_login:{
-        width: '35%',
-        height: '10%'
-    },
+  logo_login: {
+    width: '35%',
+    height: '10%'
+  },
 
-    title:{
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#fff',
-        marginBottom: 10,
-    },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
 
-    subtitle:{
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#abb1b7'
-    },
-    
-    div_text_input: {
-        backgroundColor: '#323339',
-        justifyContent: 'flex-start',
-        paddingTop: 20,
-        width: '100%',
-        gap: 10,
-      },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#abb1b7'
+  },
 
-      text_info:{
-          fontSize: 15,
-          fontWeight: 'bold',
-          color: '#fff',
-      },
+  div_text_input: {
+    backgroundColor: '#323339',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
+    width: '100%',
+    gap: 10,
+  },
 
-      input:{
-          backgroundColor: '#1e1f23',
-          borderRadius: 5,
-          width: '100%',
-          height: 40,
-          color: '#fff',
-          paddingLeft: 10,
-      },
+  text_info: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
 
-      text_link:{
-        marginTop: -5,
-        fontSize: 15,
-        color: '#02a3f3',
-        fontWeight: 'bold',
-      },
-    
-      div_text_button: {
-        display: 'flex',
-        marginTop: 20,
-        textAlign: 'center',
-        backgroundColor: '#5865f2',
-        borderRadius: 3,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-       },
-       
-       button:{
-        fontSize: 25,
-        color: '#fff',
-        textAlign: 'center',
-        paddingTop: 10,
-        paddingBottom: 10,
-       },
+  input: {
+    backgroundColor: '#1e1f23',
+    borderRadius: 5,
+    width: '100%',
+    height: 40,
+    color: '#fff',
+    paddingLeft: 10,
+  },
 
-       criarConta_DIV:{
-        paddingTop: 10,
-        width: '100%',
-       },
+  text_link: {
+    marginTop: -5,
+    fontSize: 15,
+    color: '#02a3f3',
+    fontWeight: 'bold',
+  },  
 
-       criarConta:{
-        color: '#727379',
-        textAlign: 'left',
-        fontWeight: 'bold',
-       },
+  text_link_termos: {
+    marginTop: -5,
+    fontSize: 15,
+    color: '#02a3f3',
+  },
 
-       required:{
-        paddingLeft: 50,
-        color: 'red'
-       }
-  });
+  div_text_button: {
+    display: 'flex',
+    marginTop: 20,
+    textAlign: 'center',
+    backgroundColor: '#5865f2',
+    borderRadius: 3,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  button: {
+    fontSize: 25,
+    color: '#fff',
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+
+  criarConta_DIV: {
+    paddingTop: 10,
+    width: '100%',
+  },
+
+  jaTemUmaConta_DIV: {
+    paddingTop: 30,
+    width: '100%',
+  },
+
+  criarConta: {
+    color: '#727379',
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+
+  termos_text: {
+    color: '#727379',
+    textAlign: 'left',
+  },
+
+  required: {
+    paddingLeft: 50,
+    color: 'red'
+  },
+
+  icon: {
+    marginLeft: 10,
+    fontSize: 24,
+    color: '#aaa',
+  },
+
+  password_div: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    paddingHorizontal: 14,
+  },
+
+  date_Div: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  dropList: {
+    width: '30%',
+    backgroundColor: '#1e1f23',
+    // borderColor: 'black',
+    // borderWidth: 1,
+    padding: 10,
+    margin: 5,
+  },
+
+  checkbox_Div: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 15,
+
+  },
+
+  opcionalCheckBoxText: {
+    color: '#727379',
+    textAlign: 'left',
+    fontSize: 14,
+    paddingLeft: 12,
+  },
+
+  checkBox: {
+    // backgroundColor: '#727379',
+    borderColor: '#727379',
+    borderWidth: 1,
+    height: 22,
+    width: 22,
+    borderRadius: 5,
+  },
+});
